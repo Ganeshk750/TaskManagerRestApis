d7f6affb193c7626513a73931a2f3bae5f001e23
Ganeshk750 <ganesh9ot@gmail.com>
2020-11-25 18:53:49 +0530

    # Adding Spring Simple Project With required Dependency

---
+++ angular-Task-Manager-restApi/src/main/java/com/ganesh/apis/AngularTaskManagerRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularTaskManagerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularTaskManagerRestApiApplication.class, args);
+	}
+
+}
